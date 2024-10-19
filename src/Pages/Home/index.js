917849ff14134525ce54616724975fee03fa74de
Roberto <roberto.felipe467@gmail.com>
2024-10-19 18:39:06 -0300

    .....

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,11 +1,14 @@
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, Modal, TextInput } from 'react-native';
 
 import Colors from '../../Components/Colors';
 import { useState } from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 export default function Home() {
-
-    const [ vazio, setVazio ] = useState(true);
+    
+    const [ modal, setModal ] = useState(true);
+    const [ vazio, setVazio ] = useState(false);
 
  return (
    <View style={styles.container}>
@@ -14,21 +17,46 @@ export default function Home() {
         </View>
 
         <View style={styles.content}>
-            {vazio ? <Vazio/> : (
+            {vazio ? <Vazio handleModal={setModal} /> : (
                 <Text></Text>
 
             )}
             
         </View>
+
+        <Modal transparent={true} animationType='slide' visible={modal} >
+            <View style={{height: '20%', width: '100%' }} />
+            <View style={styles.modalContent} >
+                <View style={{marginHorizontal: 20, marginTop: 15}} >
+                    <TouchableOpacity>
+                        <Icon name='close' size={24} color="#000" style={{alignSelf: 'flex-end'}}/>
+                    </TouchableOpacity>
+                    <Text style={{fontSize: 18, marginTop: 10}} >Nome da Tarefa</Text>
+                    <TextInput style={styles.input}/>
+                    <Text style={{fontSize: 18, marginTop: 10}} >Descrição</Text>
+                    <TextInput style={[styles.input]}/>
+                    <TouchableOpacity style={[styles.button, {marginTop: 24}]} >
+                        <Text style={{fontSize: 18, color: Colors.white}}>Adicionar</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </Modal>
    </View>
   );
 }
 
 function Vazio() {
+
+    const [ modal, setModal ] = useState(false);
+
+    const handleModal = () => {
+        setModal(true);
+    }
+
     return(
         <View style={{justifyContent: 'center', alignItems: 'center', height: '100%', width: '100%', gap: 24}}>
             <Text style={{fontSize: 24}} > Lista Vazia </Text>
-            <TouchableOpacity style={{backgroundColor: Colors.azul, width: '80%', paddingVertical: 8, alignItems: 'center', borderRadius: 24}} >
+            <TouchableOpacity onPress={handleModal} style={styles.button} >
                 <Text style={{fontSize: 18, color: Colors.white}} > Adicionar Tarefa </Text>
             </TouchableOpacity>
         </View>
@@ -49,7 +77,8 @@ const styles = StyleSheet.create({
     },
     titleHeader:{
         color: Colors.white,
-        fontSize: 28
+        fontSize: 28,
+        fontWeight: 'bold'
     },
     content:{
         flex: 4,
@@ -57,5 +86,26 @@ const styles = StyleSheet.create({
         borderTopRightRadius: 40,
         borderTopLeftRadius:40,
         alignItems: 'center'
+    },
+    modalContent:{
+        backgroundColor: Colors.white, 
+        height: '80%',
+        borderTopRightRadius: 40,
+        borderTopLeftRadius:40,
+    },
+    input:{
+        width: '100%',
+        height: 40,
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderRadius: 8,
+        fontSize: 18
+    },
+    button:{
+        backgroundColor: Colors.azul, width: '80%', 
+        paddingVertical: 8, 
+        alignItems: 'center', 
+        borderRadius: 24,
+        alignSelf: 'center'
     }
 })
